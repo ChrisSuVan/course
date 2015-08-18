343dd5f7a57af64947aeb6dc401a7565f7ad99f5
chrisSuVan <chrissuvan@gmail.com>
2015-08-18 08:18:08 +0800

    intial commit

---
+++ FizzBuzz/FizzBuzz/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FizzBuzz
+//
+//  Created by Jian Su on 2015-08-18.
+//  Copyright (c) 2015 Jian Su. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
