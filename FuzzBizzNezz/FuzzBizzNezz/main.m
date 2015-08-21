dabbafc7a161def67f5670d240075037131030b4
chrisSuVan <chrissuvan@gmail.com>
2015-08-21 19:04:50 +0800

    Finished Assignment 2: FuzzBizzNezz

--- FuzzBizzNezz/FuzzBizzNezz/main.m
+++ FuzzBizzNezz/FuzzBizzNezz/main.m
@@ -11,19 +11,69 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         char str[50] = {0};                  // init all to 0
-        printf("please type a Something: ");
-        scanf("%s", str);                    // read and format into the str buffer
         
+        BOOL quit = YES;
+        int Nezz = 1;
+        int Fuzz = 3;
+        int Bizz = 5;
         
-        // Create an NS foundation NSString object from the str buffer
-        NSString *userInput = [NSString stringWithUTF8String:str];
-        
-        if ([userInput isEqualToString:@"Fuzz"]) {
-            NSLog(@"ok");
+        while (quit) {
+            printf("please type a Number ");
+            scanf("%s", str);
+            NSString *userInput = [NSString stringWithUTF8String:str];
+            
+            int intVaue = [userInput intValue];
+            
+            
+            if((intVaue % Fuzz == 0) && (intVaue % Bizz == 0)) {
+                NSLog(@"FuzzBizz");
+                printf("continue? type Yes or No: ");
+                scanf("%s", str);
+                NSString *userInput = [NSString stringWithUTF8String:str];
+                NSString *upperCaseInput = [userInput uppercaseString];
+                if([upperCaseInput isEqualToString:@"NO"]) {
+                    quit = NO;
+                    break;
+                } else {
+                    continue;
+                }
+                
+            }
+            
+            if (intVaue % Fuzz == 0) {
+                NSLog(@"Fuzz");
+                printf("continue? type YES or NO: ");
+                scanf("%s", str);
+                NSString *userInput = [NSString stringWithUTF8String:str];
+                NSString *upperCaseInput = [userInput uppercaseString];
+                if([upperCaseInput isEqualToString:@"NO"]) {
+                    quit = NO;
+                }
+            }
+            
+            if(intVaue % Bizz == 0) {
+                NSLog(@"Bizz");
+                printf("continue? type Yes or No: ");
+                scanf("%s", str);
+                NSString *userInput = [NSString stringWithUTF8String:str];
+                NSString *upperCaseInput = [userInput uppercaseString];
+                if([upperCaseInput isEqualToString:@"NO"]) {
+                    quit = NO;
+                }
+            }
+            
+            if(intVaue == Nezz) {
+                NSLog(@"Nezz");
+                printf("continue? type Yes or No: ");
+                scanf("%s", str);
+                NSString *userInput = [NSString stringWithUTF8String:str];
+                NSString *upperCaseInput = [userInput uppercaseString];
+                if([upperCaseInput isEqualToString:@"NO"]) {
+                    quit = NO;
+                }
+            }
+            
         }
-        
-        // %@ calls description o object - in NSString case, prints the string
-        NSLog(@"lastName=%@", userInput);
     }
     return 0;
 }
