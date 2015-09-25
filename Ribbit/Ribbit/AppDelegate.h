869a3120eedcd866edc21f0abc67cf9c405f7663
Chris Su <zerogpm@gmail.com>
2015-09-24 22:29:40 -0700

    add self message app project

---
+++ Ribbit/Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Jian Su on 9/24/15.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/Ribbit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ribbit
+//
+//  Created by Jian Su on 9/24/15.
+//  Copyright © 2015 Jian Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
